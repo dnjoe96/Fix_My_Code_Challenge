4ea4ad5cfaac305534e2236541fa51477c21c209
dnjoe96 <donjoedbest@gmail.com>
2022-03-29 10:45:44 +0100

    square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+""" New class"""
+
 
 class square():
-    
+    """ class description """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -19,6 +20,7 @@ class square():
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
